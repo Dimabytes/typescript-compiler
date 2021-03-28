ccbb75acd83e1de540746c63316eb3d7f1dbe28c
dimabytes <dimabytes@gmail.com>
2021-03-28 21:04:48 +0300

    tests

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -3,17 +3,15 @@ import { loadAndCalculate } from './parser';
 import { END_STATEMENT } from './consts';
 import { goToNextStatement } from './utils';
 
-export class Interpreter {
-  static process(scriptData: string): number {
-    let result = 0;
-    const script = new ParsingScript(scriptData);
+export const process = (scriptData: string): number => {
+  let result = 0;
+  const script = new ParsingScript(scriptData);
 
-    while (script.stillValid()) {
-      result = loadAndCalculate(script, END_STATEMENT);
+  while (script.stillValid()) {
+    result = loadAndCalculate(script, END_STATEMENT);
 
-      goToNextStatement(script);
-    }
-
-    return result;
+    goToNextStatement(script);
   }
-}
+
+  return result;
+};
--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,47 +1,34 @@
 import path from 'path';
-import { Interpreter } from '../Interpreter';
+import { process } from '../Interpreter';
 
 const getFixturePath = (filename: string) => path.join(__dirname, '..', '__fixtures__', filename);
 // const readFile = (filename: string) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-// describe('calculateExpression', () => {
-//   test.each([
-//     ['2+2', 4],
-//     ['1+2*3', 7],
-//     ['10^3/2', 500],
-//     ['(10-5)*(5^2)', 125],
-//     ['0+0', 0],
-//   ])('expression %s is equal %i', (expression, expected) => {
-//     const calculator = new ExpressionCalculator(expression);
-//     expect(calculator.loadAndCalculate()).toBe(expected);
-//   });
-//
-//   it('calculate sin', async () => {
-//     const calculator = new ExpressionCalculator('sin(PI/2)');
-//     expect(calculator.loadAndCalculate()).toBe(1);
-//   });
-//
-//   it('calculate pow', async () => {
-//     const calculator = new ExpressionCalculator('pow(5, 2)');
-//     expect(calculator.loadAndCalculate()).toBe(25);
-//   });
-//
-//   it('calculate expression from file', async () => {
-//     const res = await calculateFileExpression(getFixturePath('expressionExample.txt'));
-//     expect(res).toBe(900);
-//   });
-//
-//   it('calculate if statement from file', async () => {
-//     const calculator = new ExpressionCalculator('if(100) {5} else {10}');
-//     expect(calculator.loadAndCalculate()).toBe(5);
-//   });
-// });
+describe('calculateExpression', () => {
+  test.each([
+    ['2+2', 4],
+    ['1+2*3', 7],
+    ['10^3/2', 500],
+    ['(10-5)*(5^2)', 125],
+    ['0+0', 0],
+  ])('expression %s is equal %i', (expression, expected) => {
+    expect(process(expression)).toBe(expected);
+  });
 
-describe('test', () => {
-  it('calculate', () => {
-    const data = 'pow(2,5)';
-    const res = Interpreter.process(data);
+  it('calculate sin', async () => {
+    expect(process('sin(PI/2)')).toBe(1);
+  });
 
-    expect(res).toBe(8);
+  it('calculate pow', async () => {
+    expect(process('pow(5, 2)')).toBe(25);
+  });
+
+  // it('calculate expression from file', async () => {
+  //   const res = await calculateFileExpression(getFixturePath('expressionExample.txt'));
+  //   expect(res).toBe(900);
+  // });
+
+  it('calculate if statement from file', async () => {
+    expect(process('if(100) {5} else {10}')).toBe(5);
   });
 });
